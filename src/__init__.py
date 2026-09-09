1484b89fb8da73fe25886ce6e6c8e257a10649a3
Jullian J. Arredondo <arredondojullianjoseph@gmail.com>
2026-09-09 17:22:20 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
 from .bateman import bateman_linear_chain
 from .chain import build_linear_chain_matrix
 from .mmpa import expm_mmpa_apply, mmpa_linear_chain
+from .radau import radau_linear_chain
 
 __all__ = [
     "bateman_linear_chain",
     "build_linear_chain_matrix",
     "expm_mmpa_apply",
     "mmpa_linear_chain",
+    "radau_linear_chain",
 ]
